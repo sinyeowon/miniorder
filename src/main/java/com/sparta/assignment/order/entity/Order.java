f719cf457215815e7c4ede52b89c7e196b6f967e
sinyeowon <ioohyou@knu.ac.kr>
2026-04-20 01:51:44 +0900

    feat: Order Entity 구현

---
+++ src/main/java/com/sparta/assignment/order/entity/Order.java
@@ -0,0 +1,35 @@
+package com.sparta.assignment.order.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.sparta.assignment.prduct.entity.Product;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Order(Product product) {
+        this.product = product;
+    }
+}
