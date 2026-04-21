5a28a346476032d84543462effd076637b62b695
sinyeowon <ioohyou@knu.ac.kr>
2026-04-22 01:33:40 +0900

    docs: Product Entity 공부한 내용 주석 추가

--- src/main/java/com/sparta/assignment/prduct/entity/Product.java
+++ src/main/java/com/sparta/assignment/prduct/entity/Product.java
@@ -27,11 +27,13 @@ public class Product {
     @Column(nullable = false)
     private Integer price;
 
+    // Product를 생성할 때, name과 price가 있어야 생성할 수 있도록 비즈니스 생성자 정의
     public Product(String name, Integer price) {
         this.name = name;
         this.price = price;
     }
 
+    // update 메서드는 자동으로 주어지지 않기 때문에 직접 정의해서 사용
     public void update(String name, Integer price) {
         this.name = name;
         this.price = price;
